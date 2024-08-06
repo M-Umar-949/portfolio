12c611b99cae091290f19e123690d5e6218c2f83
M-Umar-949 <umarrajput930@gmail.com>
2024-08-06 17:53:10 +0500

    removed prodigy

--- utils/data/experience.js
+++ utils/data/experience.js
@@ -4,11 +4,5 @@ export const experiences = [
     title: 'AI intern',
     company: "AIM lab FAST-NU",
     duration: "(Jun 2023 - Aug 2023)"
-  },
-  {
-    id: 2,
-    title: "Data Science intern",
-    company: "Prodigy Info Tech(Remote)",
-    duration: "(Jun 2024 - Jul 2024)"
   }
 ]
